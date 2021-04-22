8951cd22f2ae2d4f3638093e96ae22ed8b5d66f5
study-hary-id <haryansyah.study.id@gmail.com>
2021-04-22 14:41:47 +0700

    Delete back-end files

--- api/middlewares/errorHandler.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-unused-vars */
-const handleErrors = (err, req, res, next) => {
-  res.status(500).json({
-    status: 'error',
-    message: err.message
-  });
-};
-
-module.exports = handleErrors;
